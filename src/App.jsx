9b1716c333e5617d187546d46bfa9d1a0fca74d9
Simon-Sahlin <simonsahlin@somin.se>
2025-07-05 21:15:40 +0200

    Add basic functionality

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,28 @@
-import fish2 from './assets/fish2.jpg';
+import { useEffect, useState } from 'react';
+
+const images = import.meta.glob("./assets/*", { eager: true })
+const imageList = Object.values(images).map(mod => mod.default);
 
 function App() {
+  
+  const [showCard, updateCard] = useState(true);
+  const [cardImage, updateCardImage] = useState(imageList[1]);
+
+  useEffect(()=>{
+    function handleKeyPress(e){
+      if (e.code === "ArrowUp") {
+        updateCard(prev => !prev);
+      }
+      else if (e.code === "ArrowRight") {
+        updateCardImage(imageList[Math.floor(Math.random() * imageList.length)]);
+        updateCard(false);
+      }
+    }
+
+    window.addEventListener('keydown', handleKeyPress);
+    return () => window.removeEventListener('keydown', handleKeyPress);
+  }, [])
+  
   return (
     <>
       <div className="flex flex-col h-screen bg-zinc-800">
@@ -10,9 +32,9 @@ function App() {
         <div className="flex items-center justify-center grow">
           <div 
             className='aspect-square w-[min(80vw,80vh)] bg-cover bg-center rounded-full bg-zinc-700 flex items-center justify-center'
-            style={{ backgroundImage: `url(${fish2})` }}
+            style={{ backgroundImage: `url(${!showCard ? cardImage : ""})` }}
           >
-            <h2 className="text-white font-bold text-7xl text-center">Whats The Name? yeaheahea</h2>
+            {showCard && <h2 className="text-white font-bold text-7xl text-center">{cardImage.split("/").at(-1).split(".")[0]}</h2>}
           </div>
         </div>
         <div className="flex items-center justify-center p-4">
