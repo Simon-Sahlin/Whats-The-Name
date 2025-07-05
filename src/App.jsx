5aef6d60db2086e2b6315cab2a2f4476e975944e
Simon-Sahlin <simonsahlin@somin.se>
2025-07-05 20:47:22 +0200

    Layout and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,22 @@
-// import reactLogo from './assets/react.svg'
+import fish2 from './assets/fish2.jpg';
 
 function App() {
   return (
     <>
-      <div>
-        <div>
-          <h1>Whats The Name?</h1>
+      <div className="flex flex-col h-screen bg-zinc-800">
+        <div className="flex items-center justify-center p-7">
+          <h1 className="text-white font-bold text-4xl">Whats The Name?</h1>
         </div>
-        <div>
-          <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS-AZfkKMJ4o6__gMKp43s2MuaJxlgGRSwzUA&s" alt="" />
+        <div className="flex items-center justify-center grow">
+          <div 
+            className='aspect-square w-[min(80vw,80vh)] bg-cover bg-center rounded-full bg-zinc-700 flex items-center justify-center'
+            style={{ backgroundImage: `url(${fish2})` }}
+          >
+            <h2 className="text-white font-bold text-7xl text-center">Whats The Name? yeaheahea</h2>
+          </div>
         </div>
-        <div>
-          <p>You are doing great!</p>
+        <div className="flex items-center justify-center p-4">
+          <p className="text-slate-500 text-xl">You are doing great!</p>
         </div>
       </div>
     </>
